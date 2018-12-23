092254a4f6c7686acb2e2f9a81c8e555d8c578b6
sund@y <yinyazuolun@sina.com>
2018-12-23 15:02:56 +0800

    asidebar fix  data from json

--- src/client/components/frame/asideBar.vue
+++ src/client/components/frame/asideBar.vue
@@ -1,31 +1,24 @@
-<template>
-    <el-menu default-active="2">
-        <el-menu-item index="1">
-            <i class="el-icon-tickets"></i>
-            <span slot="title">首页</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-            <i class="el-icon-location"></i>
-            <span slot="title">目标</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-            <i class="el-icon-menu"></i>
-            <span slot="title">计划</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-            <i class="el-icon-document"></i>
-            <span slot="title">任务</span>
-        </el-menu-item>
-        <el-menu-item index="5">
-            <i class="el-icon-setting"></i>
-            <span slot="title">日程</span>
-        </el-menu-item>
+<template  >
+    <el-menu :default-active="defaultActive">
+        <el-menu v-for="navbody in navList" :key="navbody.index">
+            <nuxt-link :to="navbody.url">
+                <el-menu-item :index="navbody.index">
+                    <i :class="navbody.icon"></i>
+                    <span slot="title">{{navbody.title}}</span>
+                </el-menu-item>
+            </nuxt-link>
+        </el-menu>
     </el-menu>
 </template>
 
 <script lang="ts">
 import { Component, Inject, Model, Prop, Vue, Watch } from 'nuxt-property-decorator'
-export default class Asidebar extends Vue {
+// import { NavBody } from '../../store';
 
+const asidebarDataName: any = require('../../static/routerData.json')
+@Component({})
+export default class Asidebar extends Vue {
+navList: any[]  = asidebarDataName.asidebar
+defaultActive: string = '1'
 }
 </script>
