490a907945fcafdd74f1a6b581c9f127c7c5d517
sund@y <yinyazuolun@sina.com>
2018-12-23 15:16:53 +0800

    nuxt asidebar page

---
+++ src/client/pages/plat.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-container class="main-container">
+    <el-header>
+      <headerbar></headerbar>
+    </el-header>
+    <el-container>
+      <el-aside width="280px">
+        <asidebar></asidebar>
+      </el-aside>
+      <el-main>
+        <nuxt-child :key="$route.name"/>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import headerbar from '~/components/frame/headbar.vue'
+import asidebar from '~/components/frame/asidebar.vue'
+
+export default {
+  components: {
+    headerbar,
+    asidebar
+  }
+}
+</script>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+.main-container {
+  /* height: 100vh; */
+  height: 100%;
+  width: 100%;
+  position: fixed;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
---
+++ src/client/pages/plat/target.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+target
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+    export default class target extends Vue {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
